cdfd39f2485ec202b6e933c0dec11e55f6a1bd26
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-05-09 17:17:51 +0400

    fix search requests url bug

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,7 +3,7 @@ from .views import blood_views, search_views, user_views, chat_views
 
 urlpatterns = [
     path("blood/", blood_views.BloodTypesView.as_view(), name='get-blood-types'),
-    path("search-requests", search_views.FilterSearchRequestsView.as_view(), name='get-search-requests'),
+    path("search-requests/", search_views.FilterSearchRequestsView.as_view(), name='get-search-requests'),
     path("users/", user_views.FilterDonorsView.as_view(), name='get-users'),
     path("broadcast/", search_views.BroadcastSearchView.as_view(), name='broadcast-search-request'),
     path("chats/user/", chat_views.ChatPeopleView.as_view(), name='get-chat-people'),
