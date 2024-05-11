f45711f07d99aae000b69ee405047779bfcda7a8
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-05-11 13:12:53 +0400

    test conversation delete existing conversation

--- backend/api/tests/test_conversation_delete.py
+++ backend/api/tests/test_conversation_delete.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest.mock import MagicMock
 
 from django.test import TestCase
@@ -5,7 +6,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from api.api_models.chat_models import ConversationDeleteRequest
-from api.models import UserIcon, BloodType, User
+from api.models import UserIcon, BloodType, User, Chat
 from api.views.chat_views import ConversationDeleteView
 from test_blood_matcher import fill_blood_types
 from test_filter_users import insert_default_user
@@ -32,3 +33,12 @@ class ConversationDeleteTestCase(TestCase):
         request.data = ConversationDeleteRequest(999).as_dictionary()
         response: Response = ConversationDeleteView().delete(request)
         self.assertEquals(response.status_code, 400)
+
+    def test_delete_conversation_existing_conversation(self) -> None:
+        chat = Chat.objects.create(
+            donor=self.user_1, receiver=self.user_2, start_date=datetime.now(), valid_status=True
+        )
+        request = MagicMock(spec=Request)
+        request.data = ConversationDeleteRequest(chat.pk).as_dictionary()
+        response: Response = ConversationDeleteView().delete(request)
+        self.assertEquals(response.status_code, 204)
