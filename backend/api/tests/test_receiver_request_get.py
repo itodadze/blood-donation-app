2d26163ed8bcd88c319307d027ff9cf0fc699795
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-05-13 00:31:33 +0400

    test receiver request get existing request

--- backend/api/tests/test_receiver_request_get.py
+++ backend/api/tests/test_receiver_request_get.py
@@ -5,7 +5,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from api.api_models.request_models import ReceiverRequestGet
-from api.models import UserIcon, BloodType, User
+from api.models import UserIcon, BloodType, User, ReceiverRequest
 from api.views.request_views import ReceiverRequestView
 from test_blood_matcher import fill_blood_types
 from test_filter_users import insert_default_user
@@ -16,10 +16,8 @@ class ReceiverRequestGetTestCase(TestCase):
         self.icon: UserIcon = UserIcon.objects.create(file_address="path/to/icon")
         fill_blood_types()
         self.o_plus: BloodType = BloodType.objects.get(blood_type="O", rhesus_factor=True)
-        self.user_1: User = insert_default_user(
+        self.user: User = insert_default_user(
             self.icon, self.o_plus, "Test", "Subject1")
-        self.user_2: User = insert_default_user(
-            self.icon, self.o_plus, "Test", "Subject2")
 
     def test_get_receiver_request_incorrect_data(self) -> None:
         request = MagicMock(spec=Request)
@@ -32,3 +30,12 @@ class ReceiverRequestGetTestCase(TestCase):
         request.data = ReceiverRequestGet(15).as_dictionary()
         response: Response = ReceiverRequestView().get(request)
         self.assertEquals(response.status_code, 400)
+
+    def test_get_receiver_request_existing_request(self) -> None:
+        expected = ReceiverRequest.objects.create(
+            user=self.user, blood_type=self.o_plus, loc_longitude=5.0, loc_latitude=5.0
+        )
+        request = MagicMock(spec=Request)
+        request.data = ReceiverRequestGet(expected.pk).as_dictionary()
+        response: Response = ReceiverRequestView().get(request)
+        self.assertEquals(response.status_code, 200)
