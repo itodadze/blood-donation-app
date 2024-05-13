4f7063fece81852591ae0cc0a145bed3125f456e
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-05-13 11:19:02 +0400

    introduce get donation amount

--- backend/api/serializers/donation_serializers.py
+++ backend/api/serializers/donation_serializers.py
@@ -3,3 +3,7 @@ from rest_framework import serializers
 
 class DonationPostSerializer(serializers.Serializer):
     donor = serializers.IntegerField(allow_null=False)
+
+
+class AmountSerializer(serializers.Serializer):
+    amount = serializers.IntegerField(allow_null=False)
