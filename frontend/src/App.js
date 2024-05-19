9d319f4a80de26a595f9a550b2036592d7938ab6
kett1-3 <knose20@freeuni.edu.ge>
2024-05-19 14:11:24 +0400

    init login and register pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import {Home} from "./pages/HomePage";
 import {RequestForm} from "./pages/RequestFormPage"
 import {useState} from "react";
 import {Chat} from "./pages/ChatPage";
+import {Login} from "./pages/LoginPage";
+import {Register} from "./pages/RegisterPage";
 
 function App() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -20,6 +22,10 @@ function App() {
                                                          toggleSidebar={toggleSidebar}/>}/>
             <Route path='/chat' element={<Chat isSidebarOpen={isSidebarOpen}
                                                toggleSidebar={toggleSidebar}/>}/>
+            <Route path='/login' element={<Login isSidebarOpen={isSidebarOpen}
+                                                 toggleSidebar={toggleSidebar}/>}/>
+            <Route path='/register' element={<Register isSidebarOpen={isSidebarOpen}
+                                                       toggleSidebar={toggleSidebar}/>}/>
         </Routes>
     );
 }
--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -2,16 +2,9 @@ import React, {useState} from 'react'
 import {Helmet} from "react-helmet";
 import colors from "../values/colors";
 import menu from "../assets/icons/menu.svg";
-import {ChosenChat} from "../components/chat/ChosenChat";
-import {ConversationList} from "../components/chat/ChatConversationList";
 import {SideMenu} from "../components/SideMenu";
 
-export const Chat = ({isSidebarOpen, toggleSidebar}) => {
-    const [chosenRecipient, setRecipient] = useState(null);
-
-    const chooseRecipient = (recipient) => {
-        setRecipient(recipient);
-    };
+export const Register = ({isSidebarOpen, toggleSidebar}) => {
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', height: '100vh'}}>
@@ -22,18 +15,12 @@ export const Chat = ({isSidebarOpen, toggleSidebar}) => {
             </Helmet>
             {isSidebarOpen && <SideMenu/>}
             <div style={{display: 'flex', flexDirection: 'column', flex: '1'}}>
-                <div style={{backgroundColor: colors.tertiary, display: 'flex', flexDirection: 'row', height: '10vh'}}>
+                <div style={{backgroundColor: colors.pearl, display: 'flex', flexDirection: 'row', height: '10vh'}}>
                     <div style={{alignItems: "center", display: "flex"}}>
                         <img src={menu} alt="menu icon" width="60vh" height="52vh"
                              onClick={toggleSidebar}/>
                     </div>
                 </div>
-
-                <div style={{margin: '2vh', border: colors.tertiary, borderStyle:'dotted',  display: 'flex', flexDirection: 'row', flex: '1', backgroundColor: colors.pearl, maxHeight: '86vh'}}>
-                    <ChosenChat chosenRecipient={chosenRecipient} />
-                    <ConversationList chosenRecipient={chosenRecipient} chooseRecipient={chooseRecipient} />
-                </div>
-
             </div>
 
         </div>
