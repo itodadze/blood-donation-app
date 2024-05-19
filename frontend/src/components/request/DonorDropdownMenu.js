40592f6302332476ba31c842adaca505cc23e192
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-05-19 20:21:39 +0400

    add select donor dropdown menu

---
+++ frontend/src/components/request/DonorDropdownMenu.js
@@ -0,0 +1,41 @@
+import {Button, Dropdown} from "react-bootstrap";
+import {DropdownBloodOption} from "./DropdownBloodOption";
+import {useEffect, useState} from "react";
+import {getBloodTypes} from "../services/BloodTypeService";
+import {bloodTypeToSvg} from "../services/BloodTypeToSvg";
+import {DropdownDonorOption} from "./DropdownDonorOption";
+
+export const DonorDropdownMenu = ({users, className}) => {
+    const [donors, setDonors] = useState([]);
+
+    const handleDonor = ({user, check}) => {
+        if (check) {
+            setDonors(donors + user)
+        } else {
+            setDonors(donors.filter(function(e) { return e !== user }))
+        }
+    }
+
+    return (
+        <Dropdown>
+            <Dropdown.Toggle as={Button} variant="outline-dark" className={className}>
+                ვინ ჩააბარა სისხლი
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                {
+                    users.map(
+                        (user) => {
+                            return (
+                                <DropdownDonorOption
+                                    user = {user.id}
+                                    description = {"" + user.first_name + user.last_name}
+                                    action = {handleDonor}
+                                />
+                            );
+                        }
+                    )
+                }
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
