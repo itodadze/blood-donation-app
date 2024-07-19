bf5f9a9583161437e5b6a5de47cb441541fac738
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-20 00:09:53 +0400

    introduce information component

---
+++ frontend/src/components/help/HelpInformation.js
@@ -0,0 +1,8 @@
+import colors from "../../values/colors";
+
+export const HelpInformation = () => {
+    return (
+      <div style={{height: "90%", width: "93%", backgroundColor: colors.pearl}}>
+      </div>
+    );
+}
--- frontend/src/pages/HelpPage.js
+++ frontend/src/pages/HelpPage.js
@@ -1,6 +1,8 @@
 import React from "react";
+import chaos_background from "../assets/background/chaos_background.png";
 import {HelpPageMenu} from "../components/help/HelpPageMenu";
 import {HelpTopBar} from "../components/help/HelpTopBar";
+import {HelpInformation} from "../components/help/HelpInformation";
 
 export const Help = ({isSidebarOpen, toggleSidebar, currentUser}) => {
     return (
@@ -8,6 +10,10 @@ export const Help = ({isSidebarOpen, toggleSidebar, currentUser}) => {
             {isSidebarOpen && <HelpPageMenu currentUser={currentUser}/>}
             <div style={{flex: '1', display: 'flex', flexDirection: 'column', width: "100%"}}>
                 <HelpTopBar toggleSidebar={toggleSidebar}/>
+                <div style={{backgroundImage: `url(${chaos_background})`, width: "100%", height: "100%",
+                alignItems: "center", justifyContent: "center", display: 'flex', flexDirection: 'column'}}>
+                    <HelpInformation/>
+                </div>
             </div>
         </div>
     )
