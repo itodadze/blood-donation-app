53443dfe9ef23daab16f04b7188f45972234c6bb
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-20 01:04:09 +0400

    fill out help information

--- frontend/src/components/help/HelpTopBar.js
+++ frontend/src/components/help/HelpTopBar.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export const HelpTopBar = ({toggleSidebar}) => {
     return (
-        <div style={{flex: '0 0 0', backgroundColor: colors.dark_pearl, minHeight: '80px'}}>
+        <div style={{flex: '0 0 0', backgroundColor: colors.dark_pearl, minHeight: '10%'}}>
             <div style={{display: 'flex', flexDirection: 'row', height: '80px'}}>
                 <div style={{alignItems: "center", display: "flex"}}>
                     <img src={menu} alt="menu icon" width="60vh" height="52vh"
--- frontend/src/pages/HelpPage.js
+++ frontend/src/pages/HelpPage.js
@@ -6,15 +6,19 @@ import {HelpInformation} from "../components/help/HelpInformation";
 
 export const Help = ({isSidebarOpen, toggleSidebar, currentUser}) => {
     return (
-        <div style={{display: 'flex', flexDirection: 'row', height: '100vh'}}>
-            {isSidebarOpen && <HelpPageMenu currentUser={currentUser}/>}
-            <div style={{flex: '1', display: 'flex', flexDirection: 'column', width: "100%"}}>
-                <HelpTopBar toggleSidebar={toggleSidebar}/>
-                <div style={{backgroundImage: `url(${chaos_background})`, width: "100%", height: "100%",
-                alignItems: "center", justifyContent: "center", display: 'flex', flexDirection: 'column'}}>
-                    <HelpInformation/>
+        <div style={{ display: 'flex', flexDirection: 'row', height: '100vh', maxHeight: '100vh' }}>
+            {isSidebarOpen && <HelpPageMenu currentUser={currentUser} />}
+            <div style={{ flex: '1', display: 'flex', flexDirection: 'column', width: "100%", maxHeight: '100%' }}>
+                <HelpTopBar toggleSidebar={toggleSidebar} />
+                <div style={{
+                    backgroundImage: `url(${chaos_background})`, width: "100%", height: "90%",
+                    display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', maxHeight: '100%'
+                }}>
+                    <div style={{ width: "100%", height: "100%", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <HelpInformation />
+                    </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
