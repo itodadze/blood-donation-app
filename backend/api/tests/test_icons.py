253319ea3f9f212282b1022b9f03e1e02d6ee531
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-21 18:08:34 +0400

    test set user icon view

--- backend/api/tests/test_icons.py
+++ backend/api/tests/test_icons.py
@@ -1,15 +1,49 @@
+
 from unittest.mock import MagicMock
 
 from django.test import TestCase
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from api.views.icon_views import UserIconView
+from api.api_models.icon_models import IconSet
+from api.models import UserIcon, BloodType
+from api.views.icon_views import UserIconView, SetUserIconView
+from test_filter_users import insert_default_user
 
 
 class UserIconsTestCase(TestCase):
+    def setUp(self):
+        self.icon = UserIcon.objects.get(file_address="icon_1")
+        self.blood = BloodType.objects.get(narrative="პირველი უარყოფითი")
+        self.user = insert_default_user(self.icon, self.blood,
+                                        "Test", "Subject")
+
     def test_icons_get(self) -> None:
         request = MagicMock(spec=Request)
         response: Response = UserIconView().get(request)
         assert response.status_code == 200
         assert len(response.data) == 10
+
+    def test_set_icon_invalid_request(self) -> None:
+        request = MagicMock(spec=Request)
+        request.data = 3
+        response: Response = SetUserIconView().post(request)
+        assert response.status_code == 400
+
+    def test_set_icon_invalid_user(self) -> None:
+        request = MagicMock(spec=Request)
+        request.data = IconSet(999, self.icon.pk).as_dictionary()
+        response: Response = SetUserIconView().post(request)
+        assert response.status_code == 404
+
+    def test_set_icon_invalid_icon(self) -> None:
+        request = MagicMock(spec=Request)
+        request.data = IconSet(self.user.pk, 999).as_dictionary()
+        response: Response = SetUserIconView().post(request)
+        assert response.status_code == 404
+
+    def test_set_icon_valid_request(self) -> None:
+        request = MagicMock(spec=Request)
+        request.data = IconSet(self.user.pk, self.icon.pk).as_dictionary()
+        response: Response = SetUserIconView().post(request)
+        assert response.status_code == 200
