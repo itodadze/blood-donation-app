7bd95d4710784929312b09887c35d903aaa3b2e2
kett1-3 <knose20@freeuni.edu.ge>
2024-07-24 21:58:32 +0400

    add check selected blood while registering

--- frontend/src/pages/RegisterMedInfo.js
+++ frontend/src/pages/RegisterMedInfo.js
@@ -12,12 +12,7 @@ export const RegisterMedInfo = ({setCurrentUser}) => {
 
     const location = useLocation();
     const {
-        selectedFirstName,
-        selectedLastName,
-        selectedEmail,
-        selectedPassword,
-        selectedPasswordConfirm,
-        selectedDate
+        selectedFirstName, selectedLastName, selectedEmail, selectedPassword, selectedPasswordConfirm, selectedDate
     } = location.state || {};
 
     const [medInfo, setInputInfo] = useState(true);
@@ -25,6 +20,8 @@ export const RegisterMedInfo = ({setCurrentUser}) => {
     const [selectedLat, setSelectedLat] = useState(null);
     const [selectedLon, setSelectedLon] = useState(null);
     const [showPopup, setShowPopup] = useState(false);
+    const [errorTxt, setErrorTxt] = useState('');
+
 
     const navigate = useNavigate();
 
@@ -32,25 +29,31 @@ export const RegisterMedInfo = ({setCurrentUser}) => {
         setInputInfo(!medInfo)
     }
     const handleSelect = (eventKey, event) => {
+        if (errorTxt) {
+            setErrorTxt('');
+        }
         setSelectedBlood(eventKey.id);
     };
-    const handleRegister =  async (e) => {
+
+    const handleRegister = async (e) => {
         e.preventDefault();
-        try {
-            const user = await register(selectedFirstName, selectedLastName, selectedEmail, selectedPassword,
-                selectedPasswordConfirm, selectedDate, selectedLat, selectedLon, selectedBlood,
-                medInfo)
-            setCurrentUser(user);
-            navigate('/');
-        } catch (error) {
-            console.error('Registration error', error);
-            // error message
+        if (medInfo && !selectedBlood) {
+            setErrorTxt('თუ დონორად რეგისტრირდებით, გთხოვთ აირჩიოთ სისხლის ტიპი');
+        } else {
+            try {
+                const user = await register(selectedFirstName, selectedLastName, selectedEmail, selectedPassword, selectedPasswordConfirm, selectedDate, selectedLat, selectedLon, selectedBlood, medInfo)
+                setCurrentUser(user);
+                navigate('/');
+            } catch (error) {
+                setErrorTxt(error);
+            }
         }
 
     };
     const onPopupClose = () => {
         setShowPopup(false)
     }
+
     const onPopupOpen = () => {
         setShowPopup(true)
     }
@@ -71,66 +74,76 @@ export const RegisterMedInfo = ({setCurrentUser}) => {
             position: 'relative',
             backgroundImage: `url(${chaos_background})`
         }}>
-            {
-                showPopup && <div className={"register-popup-envelope"}>
-                    <div className={"register-popup"}>
-                        <div className="weak-close-button" onClick={onPopupClose}>X</div>
-                        <LocationPick setSelectedLat={setSelectedLat} setSelectedLon={setSelectedLon}
-                                      className={"register-location"} latitude={selectedLat} longitude={selectedLon}/>
-                    </div>
+            {showPopup && <div className={"register-popup-envelope"}>
+                <div className={"register-popup"}>
+                    <div className="weak-close-button" onClick={onPopupClose}>X</div>
+                    <LocationPick setSelectedLat={setSelectedLat} setSelectedLon={setSelectedLon}
+                                  className={"register-location"} latitude={selectedLat} longitude={selectedLon}/>
                 </div>
-            }
+            </div>}
 
-            {
-                !showPopup && <div style={{
-                    backgroundImage: `url(${chaos_background})`,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    height: '100%',
-                    maxHeight: '100%',
-                    maxWidth: '100%',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}>
-                    <div className={"register-med-box"}>
-                        <div className={"register-box-head"}>
-                            <p style={{color: colors.primary_dark, marginBottom: '0px'}}>დონორობისთვის
-                            საჭირო ინფორმაცია</p>
-                        </div>
+            {!showPopup && <div style={{
+                backgroundImage: `url(${chaos_background})`,
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+                maxHeight: '100%',
+                maxWidth: '100%',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <div id={'register-med-error-box'}
+                     className="register-med-error-box"
+                     style={{
+                         '--background-color': errorTxt ? colors.dark_pearl : 'transparent',
+                         '--border-color': errorTxt ? colors.primary : 'transparent'
+                     }}>
+                    <span style={{textAlign: 'center', color: colors.blood, fontWeight: 'bold'}}>
+                        {errorTxt}
+                    </span>
+                </div>
 
-                        <div style={{height: '50px', marginTop: '5px', width: '100%'}}>
-                            <label
-                                style={{width: '100%', padding: '2%', display: 'flex', justifyContent: 'center'}}>
-                                <input type={"checkbox"} onInput={handleMedInfo}
-                                           style={{
-                                               width: '20px',
-                                               height: '25px',
-                                               marginLeft: '2%',
-                                               marginRight: '2%'
-                                }}/>
-                                <span>არ მსურს დონორობა</span>
-                            </label>
-                        </div>
+                <div className={"register-med-box"}>
+                    <div className={"register-box-head"}>
+                        <p style={{color: colors.primary_dark, marginBottom: '0px'}}>
+                            დონორობისთვის საჭირო ინფორმაცია
+                        </p>
+                    </div>
 
-                        <div style={{height: '50px', marginTop: '5px', width: '100%'}}>
-                            <label className={"register-box-inner"}>
-                                {medInfo && <div style={{width: '100%', height: '100%', alignItems: 'center',
-                                justifyContent: 'center', display: 'flex', flexDirection: 'column'}}>
-                                    <BloodDropdownMenu handleSelect={handleSelect}
-                                                           className={"med-dropdown-menu"}/>
-                                    <ClickableButton buttonText={"აირჩიე ლოკაცია"} onClick={onPopupOpen}/>
-                                </div>}
-                                {!medInfo &&
-                                    <span style={{margin: '0px 10px', textAlign: 'center', color: colors.blood}}>
+                    <div style={{height: '50px', marginTop: '5px', width: '100%'}}>
+                        <label
+                            style={{width: '100%', padding: '2%', display: 'flex', justifyContent: 'center'}}>
+                            <input type={"checkbox"} onInput={handleMedInfo}
+                                   style={{
+                                       width: '20px', height: '25px', marginLeft: '2%', marginRight: '2%'
+                                   }}/>
+                            <span>არ მსურს დონორობა</span>
+                        </label>
+                    </div>
+
+                    <div style={{height: '50px', marginTop: '5px', width: '100%'}}>
+                        <label className={"register-box-inner"}>
+                            {medInfo && <div style={{
+                                width: '100%',
+                                height: '100%',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                display: 'flex',
+                                flexDirection: 'column'
+                            }}>
+                                <BloodDropdownMenu handleSelect={handleSelect}
+                                                   className={"med-dropdown-menu"}/>
+                                <ClickableButton buttonText={"აირჩიე ლოკაცია"} onClick={onPopupOpen}/>
+                            </div>}
+                            {!medInfo && <span style={{margin: '0px 10px', textAlign: 'center', color: colors.blood}}>
                                         თქვენ არ დარეგისტირდებით დონორად, მომავალში ცვლილებისთვის შეგიძლიათ შეცვალოთ პროფილის გვერდზე</span>}
-                                <ClickableButton buttonText={"დარეგისტრირდი"} onClick={(e) => {
-                                    handleRegister(e)
-                                }}/>
-                            </label>
-                        </div>
+                            <ClickableButton buttonText={"დარეგისტრირდი"} onClick={(e) => {
+                                handleRegister(e)
+                            }}/>
+                        </label>
                     </div>
                 </div>
-            }
+            </div>}
 
         </div>
 
