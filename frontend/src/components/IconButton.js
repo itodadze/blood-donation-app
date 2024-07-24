694eeb9e7d3532e755dfe8ac04960eb4d09c89a7
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-24 23:48:38 +0400

    add on click to icon button

--- frontend/src/components/IconButton.js
+++ frontend/src/components/IconButton.js
@@ -1,8 +1,8 @@
 import colors from "../values/colors";
 
-export const IconButton = ({svg_file, height, text}) => {
+export const IconButton = ({svg_file, height, text, onClick}) => {
     return (
-        <div className={"home-menu-button"} style={{backgroundColor: colors.pearl}}>
+        <div className={"home-menu-button"} style={{backgroundColor: colors.pearl}} onClick={onClick}>
             <img src={svg_file} alt="profile icon" width="58vh" height={height}/>
             <text className={"noto-sans-georgian-elegant"} style={{
                 color: colors.blood,
