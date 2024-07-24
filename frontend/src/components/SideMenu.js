3a23fec3a73a6b34503afd671415267999244105
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-24 23:48:56 +0400

    introduce logout side menu button

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -6,7 +6,7 @@ import feed from "../assets/icons/feed.svg"
 import profile from "../assets/icons/profile.svg";
 import chat from "../assets/icons/chat.svg";
 import blood from "../assets/icons/blood.svg";
-import settings from "../assets/icons/settings.svg";
+import logout from "../assets/icons/logout.svg";
 import support from "../assets/icons/support.svg";
 import logo from "../assets/icons/logo.svg"
 import React from "react";
@@ -29,7 +29,7 @@ export const SideMenu = ({current, currentUser}) => {
             animate={{x: 0}}
             transition={{type: 'tween', ease: 'easeOut', duration: 0}}>
             <IconButton svg_file={logo} height={"60vh"} text={"DonorStream"}
-                            onClick={() => navigate("/", {currentUser: currentUser})} />
+                            onClick={() => navigate("/", {currentUser: currentUser})}/>
             {(current !== strings.HOME) &&
                 <MenuButton svg_file={feed} height={"42vh"} text={"მთავარი"}
                             onClick={() => navigate("/", {currentUser: currentUser})}/>}
@@ -47,7 +47,7 @@ export const SideMenu = ({current, currentUser}) => {
 
 
             <div style={{marginTop: "auto"}}>
-                <MenuButton svg_file={settings} height={"50vh"} text={"პარამეტრები"}/>
+                <MenuButton svg_file={logout} height={"44vh"} text={"გასვლა"}/>
             </div>
             {(current !== strings.HELP) &&
                 <MenuButton svg_file={support} height={"40vh"}
