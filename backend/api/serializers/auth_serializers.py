d160b8b745cea1ee2ba7e723eb537a663c3c31c0
kett1-3 <knose20@freeuni.edu.ge>
2024-07-25 00:19:40 +0400

    add backend validations for password and birthday

--- backend/api/serializers/auth_serializers.py
+++ backend/api/serializers/auth_serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
-
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from api.models import User
 
 
 class RegisterUserSerializer(serializers.ModelSerializer):
     birthday = serializers.DateField(input_formats=["%d/%m/%Y"])
+    password = serializers.CharField(write_only=True)
+    password_confirm = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
@@ -18,9 +21,30 @@ class RegisterUserSerializer(serializers.ModelSerializer):
             "blood_type",
             "donor_status",
             "password",
+            "password_confirm",
         ]
 
+    def validate_birthday(self, value):
+        from datetime import date
+        today = date.today()
+        if value >= today:
+            raise serializers.ValidationError("Birthday must be in the past.")
+        return value
+
+    def validate_password(self, value):
+        try:
+            validate_password(value)
+        except ValidationError as e:
+            raise serializers.ValidationError(e.messages)
+        return value
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError({"password_confirm": "Passwords do not match."})
+        return data
+
     def create(self, validated_data):
+        validated_data.pop("password_confirm")
         password = validated_data.pop("password")
         user = User.objects.create(**validated_data)
         user.set_password(password)
