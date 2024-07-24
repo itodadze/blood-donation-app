070637eea9356538eef12dfa44dd23f5ff21f09b
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-24 23:41:11 +0400

    jump to home page when clicking on logo

--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -28,7 +28,8 @@ export const SideMenu = ({current, currentUser}) => {
             initial={{x: '-100%'}}
             animate={{x: 0}}
             transition={{type: 'tween', ease: 'easeOut', duration: 0}}>
-            <IconButton svg_file={logo} height={"60vh"} text={"DonorStream"} onClick={() => {}}/>
+            <IconButton svg_file={logo} height={"60vh"} text={"DonorStream"}
+                            onClick={() => navigate("/", {currentUser: currentUser})} />
             {(current !== strings.HOME) &&
                 <MenuButton svg_file={feed} height={"42vh"} text={"მთავარი"}
                             onClick={() => navigate("/", {currentUser: currentUser})}/>}
