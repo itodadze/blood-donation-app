d5f493ce2b97e17eaca11e914ef6d1e18aadc224
Irakli Todadze <itoda20@freeuni.edu.ge>
2024-07-25 21:43:24 +0400

    use smtp as default email host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 ]
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
