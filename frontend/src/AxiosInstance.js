f37f43682acb22514143e381006f387205d5f1cb
kett1-3 <knose20@freeuni.edu.ge>
2024-07-26 01:40:36 +0400

    axios value fix

--- frontend/src/AxiosInstance.js
+++ frontend/src/AxiosInstance.js
@@ -7,7 +7,7 @@ const getCsrfToken = () => {
 
 const api = axios.create({
     baseURL: 'http://localhost:8000/api',
-    withCredentials: true.valueOf(),
+    withCredentials: true,
     headers: {
         'X-CSRFToken': getCsrfToken(),
     }
