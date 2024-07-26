3c740f50752cdadf0d305cd26b85c3f4c179e048
TamtiGior <tgior20@freeuni.edu.ge>
2024-07-26 13:55:15 +0400

    fix on click

--- frontend/src/components/MenuButton.js
+++ frontend/src/components/MenuButton.js
@@ -3,15 +3,11 @@ import colors from "../values/colors";
 export const MenuButton = ({svg_file, height, text, onClick, selected}) => {
     let color = colors.pearl
     if (selected) {
-        color = colors.gray
-    }
-    let clickBehavior = onClick
-    if (!selected) {
-        clickBehavior = {}
+        color = colors.tertiary
     }
     return (
         <div className={"home-menu-button"} style={{backgroundColor: color}}
-        onClick={clickBehavior}>
+        onClick={onClick}>
             <img src={svg_file} alt="profile icon" width="52vh" height={height}/>
             <text className={"noto-sans-georgian-elegant"} style={{
                 color: colors.secondary_dark,
