ccf1689ce8b4758a04576eec4649c843d06412cd
TamtiGior <tgior20@freeuni.edu.ge>
2024-07-26 11:11:20 +0400

    add user view

--- backend/api/views/user_views.py
+++ backend/api/views/user_views.py
@@ -5,9 +5,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.api_models.search_models import FilterRequest
+from api.api_models.user_models import UserUpdateRequest
 from api.core.blood_matcher import all_blood_types, all_donors
-from api.models import BloodType, Chat, User
-from api.serializers.user_serializers import UserSerializer
+from api.models import BloodType, Chat, User, UserIcon
+from api.serializers.user_serializers import UserSerializer, UserResponseSerializer, UserUpdateRequestSerializer
 
 
 class FilterDonorsView(APIView):
@@ -43,3 +44,41 @@ class ReceiverDonorUsersView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response("User does not exist", status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserView(APIView):
+    def get(self, request: Request) -> Response:
+        identifier = request.query_params.get("id")
+        try:
+            user = User.objects.get(pk=identifier)
+            serializer = UserResponseSerializer(user, many=False)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response("User does not exist", status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request: Request) -> Response:
+        serializer = UserUpdateRequestSerializer(data=request.data)
+        if serializer.is_valid():
+            updates: UserUpdateRequest = UserUpdateRequest(
+                **serializer.validated_data
+            )
+            user: QuerySet = User.objects.filter(pk=updates.id)
+            if updates.email:
+                user.update(email=updates.email)
+            if updates.loc_longitude and updates.loc_latitude:
+                user.update(loc_longitude=updates.loc_longitude, loc_latitude=updates.loc_latitude)
+            if updates.first_name:
+                user.update(first_name=updates.first_name)
+            if updates.last_name:
+                user.update(last_name=updates.last_name)
+            if updates.description:
+                user.update(description=updates.description)
+            try:
+                if updates.icon_id:
+                    icon = UserIcon.objects.get(pk=updates.icon_id)
+                    user.update(icon=icon)
+            except UserIcon.DoesNotExist:
+                return Response("Icon does not exist", status=status.HTTP_404_NOT_FOUND)
+            return Response(None, status=status.HTTP_200_OK)
+        else:
+            return Response("Update request invalid", status=status.HTTP_400_BAD_REQUEST)
