14252c8ff17a18a1992d2ca2a03cbed8195c0685
TamtiGior <tgior20@freeuni.edu.ge>
2024-07-26 13:50:05 +0400

    fix menu button

--- frontend/src/components/MenuButton.js
+++ frontend/src/components/MenuButton.js
@@ -1,9 +1,17 @@
 import colors from "../values/colors";
 
-export const MenuButton = ({svg_file, height, text, onClick}) => {
+export const MenuButton = ({svg_file, height, text, onClick, selected}) => {
+    let color = colors.pearl
+    if (selected) {
+        color = colors.gray
+    }
+    let clickBehavior = onClick
+    if (!selected) {
+        clickBehavior = {}
+    }
     return (
-        <div className={"home-menu-button"} style={{backgroundColor: colors.pearl}}
-        onClick={onClick}>
+        <div className={"home-menu-button"} style={{backgroundColor: color}}
+        onClick={clickBehavior}>
             <img src={svg_file} alt="profile icon" width="52vh" height={height}/>
             <text className={"noto-sans-georgian-elegant"} style={{
                 color: colors.secondary_dark,
